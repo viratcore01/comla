34ea79879d9125de6d66c72c8e6d3d39b9a1c11a
viratcore01 <viratcore01@gmail.com>
2025-10-23 05:19:30 +0530

    Convert auth middleware to ES modules

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
@@ -19,4 +19,4 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
-module.exports = { authenticateToken };
+export { authenticateToken };
