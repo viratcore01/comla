b7273c31e9a6a4ba0fdff0e040fe469bb808da82
viratcore01 <viratcore01@gmail.com>
2025-10-23 04:50:53 +0530

    Convert authController.js to ES modules

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,9 @@
-exports.signup = (req, res) => {
+export const signup = (req, res) => {
   console.log("Signup controller called");
   res.json({ message: "Signup route working!" });
 };
 
-exports.login = (req, res) => {
+export const login = (req, res) => {
   console.log("Login controller called");
   res.json({ message: "Login route working!" });
 };
