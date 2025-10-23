9b0da67880833b5cb86d7fac1d5f22cdd888dfab
viratcore01 <viratcore01@gmail.com>
2025-10-23 05:51:37 +0530

    Remove duplicate login route in authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,7 +9,6 @@ const { signup, login } = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
 
 // Login
 router.post("/login", [
